4920478d952863d7c9d98df972d2a3ac015663bd
夜雨 <yeyu54876@gmail.com>
2026-02-24 23:53:13 +0800

    feat: 添加天依相册配置及相关图片

--- src/config/galleryConfig.ts
+++ src/config/galleryConfig.ts
@@ -14,12 +14,12 @@ export const galleryConfig: GalleryConfig = {
 		// tags: 相册标签，用于分类和过滤
 		// 每添加一个数组项就相当于添加了一个相册，记得在 public/gallery/ 目录下创建对应的子目录并放入图片
 		{
-			id: "firefly-2026",
-			name: "可爱流萤",
-			description: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
-			location: "崩坏：星穹铁道",
-			date: "2026-01-01",
-			tags: ["崩坏星穹铁道", "流萤"],
+			id: "Tianyi-2026",
+			name: "可爱天依",
+			description: "华风夏韵，洛水天依。",
+			location: "VOCALOID",
+			date: "2026-01-31",
+			tags: ["VOCALOID", "天依"],
 		},
 	],
 
